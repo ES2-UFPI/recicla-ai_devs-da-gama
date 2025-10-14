7c2f3903b672403299ac17d78ceef1c4523b7aee
Peu <emanuelpedro381@gmail.com>
2025-10-14 09:00:56 -0300

    criando model e schema de resíduos

--- backend/src/infra/database/models/models.py
+++ backend/src/infra/database/models/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import Optional
+from datetime import datetime
 from bson import ObjectId
 
 class PyObjectId(ObjectId):
@@ -26,6 +27,18 @@ class User(BaseModel):
     role_id: str
     cidade_id: str
     estado_id: str
+
+    class Config:
+        allow_population_by_field_name = True
+        json_encoders = {ObjectId: str}
+
+class Residue(BaseModel):
+    id: Optional[PyObjectId] = Field(default=None, alias="_id")
+    quantidade: float
+    dataCadastro: datetime = Field(default_factory=datetime.utcnow)
+    foto: Optional[str] = None  # URL ou caminho da foto
+    categoriaId: str
+    produtorId: str
 
     class Config:
         allow_population_by_field_name = True
