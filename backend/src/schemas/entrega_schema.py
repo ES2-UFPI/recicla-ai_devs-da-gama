bf1da06ed822ac6fb2498b45c205960465fb0697
Gabriel Lopes Bastos <73184283+G4brielLB@users.noreply.github.com>
2025-11-13 08:52:07 -0300

    model de entrega, base para schema e criação do arquivo entrega repo #128

---
+++ backend/src/schemas/entrega_schema.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field, field_validator
+from typing import Optional
+from datetime import datetime
+
+class EntregaCreate(BaseModel):
+    pass
+
+class EntregaResponse(BaseModel):
+    pass
