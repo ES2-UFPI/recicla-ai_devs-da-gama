dfb29a49cee2c581d3fbe6b67c9cc6b71bd380c1
Gabriel Lopes Bastos <73184283+G4brielLB@users.noreply.github.com>
2025-11-17 07:07:04 -0300

    remoção de campo desnecessário em um schema #127

--- backend/src/schemas/return_schema.py
+++ backend/src/schemas/return_schema.py
@@ -1,12 +1,7 @@
 from pydantic import BaseModel, Field
-from typing import Optional, List
 
 
 class UserPublic(BaseModel):
 	name: str = Field(..., description="Nome do usuário")
 	email: str = Field(..., description="Email do usuário")
 	role_id: str = Field(..., description="Papel do usuário")
-	accepted_material: Optional[List[str]] = Field(None, description="Materiais aceitos pela receptora")
-	inventory: Optional[List[str]] = Field(None, description="Inventário do coletor")
-	phone: Optional[str] = Field(None, description="Telefone do usuário")
-	addresses: Optional[List[dict]] = Field(None, description="Endereços do usuário")
